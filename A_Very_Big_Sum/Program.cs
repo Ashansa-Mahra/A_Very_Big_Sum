9964d2075b57981c908b0217fe2e3c0d32f5e5ee
Ashansa Mahra <mahraashansa1997@gmail.com>
2024-01-03 19:16:43 +0530

    Add files via upload

---
+++ A_Very_Big_Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿class Result
+{
+    public static long VeryBigSum(List<long> ar)
+    {
+        return ar.Sum();
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<long> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList();
+
+        long result = Result.VeryBigSum(ar);
+
+        Console.WriteLine(result);
+    }
+}
